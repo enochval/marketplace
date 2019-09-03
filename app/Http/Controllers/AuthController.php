34062394dd96912a7614c4252874d20959f94bb0
Osarenren Enoch <osarenrenenoch@gmail.com>
2019-09-03 12:06:01 +0100

    update doc

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -67,7 +67,7 @@ class AuthController extends Controller
      *              @OA\Property(
      *                  property="phone",
      *                  description="",
-     *                  type="integer",
+     *                  type="string",
      *              ),
      *              @OA\Property(
      *                  property="password",
@@ -255,7 +255,7 @@ class AuthController extends Controller
      *              @OA\Property(
      *                  property="phone",
      *                  description="",
-     *                  type="integer",
+     *                  type="string",
      *              ),
      *              @OA\Property(
      *                  property="password",
